93a53459c25a11f879db35447836834862cebf6a
PETER ONYAM <PETERONYAM@gmail.com>
2022-09-20 13:42:31 +0100

    1.42pm

---
+++ cs50 wpyjavascript/css-my site/node.js
@@ -0,0 +1,12 @@
+let myapples = 5;
+
+let mapples = 0;
+ if(mapples < myapples){
+    mapples = 8;
+    console.log("you currently have " mapples "apples")
+ }else{
+    mapples = 0;
+    console.log("you now have " mapples "apples")
+ }
+
+
